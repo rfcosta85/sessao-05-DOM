9b81bba908f1e3253927697455edd62f2ff000eb
Rodrigo Fernandes <rfcosta85@gmail.com>
2026-09-21 13:14:43 +0100

    Exemplos tagName

---
+++ tagScript.js
@@ -0,0 +1,10 @@
+const paragrafos = document.getElementsByTagName('p');
+
+for (let i = 0; i < paragrafos.length; i++) {
+    paragrafos[i].style.color = 'blue';
+    paragrafos[i].style.fontSize = '18px';
+}
+paragrafos[2].style.color = '#00ff08';
+
+const novoTituloH2 = document.
+getElementsByTagName("h2");
